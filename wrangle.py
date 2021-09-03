e97c5d5d0fda1c01d55fd572f3a8d759fff6900f
Johnathon Smith <smith.johnathon01@gmail.com>
2021-09-03 16:42:28 -0500

    Made a few changes to my wrangle methods and updated the appropriate files

--- wrangle.py
+++ wrangle.py
@@ -26,8 +26,17 @@ def remove_outliers(df, k, col_list):
         
     return df
 
+def get_dists(df):
+    #Plot the individual distributions again
+    for col in df.columns:
+        sns.histplot(x = col, data = df)
+        plt.title(col)
+        plt.show()
+
+
 #The following function will be used in the prep functions to return the train, validate, and test splits
-def train_validate_test_split(df, target, seed = 123):
+#No need for a target variable, since stratifying a continuous variable won't really work.
+def train_validate_test_split(df, seed = 123):
     '''
     This function takes in a dataframe, the name of the target variable
     (for stratification purposes), and an integer for a setting a seed
@@ -37,12 +46,10 @@ def train_validate_test_split(df, target, seed = 123):
     The function returns, in this order, train, validate and test dataframes. 
     '''
     train_validate, test = train_test_split(df, test_size=0.2, 
-                                            random_state=seed, 
-                                            stratify=df[target])
+                                            random_state=seed)
     
     train, validate = train_test_split(train_validate, test_size=0.3, 
-                                       random_state=seed,
-                                       stratify=train_validate[target])
+                                       random_state=seed)
     return train, validate, test
 
 
@@ -63,24 +70,32 @@ def wrangle_zillow():
     #Begin by removing the null values
     zillow.dropna(inplace = True)
 
+    #I also want to rename the columns to be more understandable and easier to read
+    zillow.rename(columns = {'bedroomcnt':'bedroom_count',
+                        'bathroomcnt':'bathroom_count',
+                        'calculatedfinishedsquarefeet':'area (sq-ft)',
+                        'taxvaluedollarcnt':'tax_value',
+                        'yearbuilt':'year_built',
+                        'taxamount':'tax_amount'}, inplace = True)
+
     #Now remove things that don't make sense and/or are impossible/illegal.
     #If something didn't sound like the average 'single family residential' property, I dropped it.
-    zillow = zillow[(zillow.bedroomcnt > 0) & (zillow.bathroomcnt > 0)]
-    zillow = zillow[zillow.bedroomcnt <= 5]
-    zillow = zillow[zillow.bathroomcnt <= 3]
-    zillow = zillow[zillow.calculatedfinishedsquarefeet <= 5000]
-    zillow = zillow[zillow.calculatedfinishedsquarefeet >= (120 * zillow.bedroomcnt)]
-    zillow = zillow[zillow.taxamount <= 20_000]
+    zillow = zillow[(zillow.bedroom_count > 0) & (zillow.bathroom_count> 0)]
+    zillow = zillow[zillow.bedroom_count <= 5]
+    zillow = zillow[zillow.bathroom_count <= 3]
+    zillow = zillow[zillow['area (sq-ft)'] <= 5000]
+    zillow = zillow[zillow['area (sq-ft)'] >= (120 * zillow.bedroom_count)]
+    zillow = zillow[zillow.tax_amount <= 20_000]
 
-    #Now convert bedroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, fips, and yearbuilt to ints
-    zillow.bedroomcnt = zillow.bedroomcnt.astype(int)
-    zillow.calculatedfinishedsquarefeet = zillow.calculatedfinishedsquarefeet.astype(int)
-    zillow.taxvaluedollarcnt = zillow.taxvaluedollarcnt.astype(int)
-    zillow.fips = zillow.fips.astype(int)
-    zillow.yearbuilt = zillow.yearbuilt.astype(int)
+    #Now convert bedroomcnt, calculatedfinishedsquarefeet, and taxvaluedollarcnt to ints
+    zillow.bedroom_count = zillow.bedroom_count.astype(int)
+    zillow['area (sq-ft)'] = zillow['area (sq-ft)'].astype(int)
+    zillow.tax_value = zillow.tax_value.astype(int)
 
-    #Rename calculatedfinishedsquarefeet and taxvaluedollarcnt
-    zillow.rename(columns = {'calculatedfinishedsquarefeet':'home_area (sq-ft)', 'taxvaluedollarcnt':'home_value'}, inplace = True)
+    #fips and yearbuilt seem to be categorical, so I'm going to change those to object types
+    #That should be helpful when exploring the data
+    zillow.fips = zillow.fips.astype(object)
+    zillow.year_built = zillow.year_built.astype(object)
 
     #Now remove any outliers and return the prepared dataframe
     zillow = remove_outliers(zillow, 2.5, zillow.columns)
